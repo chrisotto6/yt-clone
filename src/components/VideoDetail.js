15a74044a8d867233d39013f7c2d45891a2375bd
Chris Otto <chris.otto6@gmail.com>
2019-08-14 10:02:48 -0500

    Updating the video components

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoDetail = () => {
+    return (<h1>VideoDetail Component</h1>)
+}
+
+export default VideoDetail;
---
+++ src/components/VideoList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoList = () => {
+    return (<h1>VideoList Component</h1>)
+}
+
+export default VideoList;
