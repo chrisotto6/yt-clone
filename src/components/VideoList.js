a78107830b99b6ec32765acabdb86cbba10267d2
Chris Otto <chris.otto6@gmail.com>
2019-08-14 21:14:07 -0500

    Finishing the VideoList component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import VideoItem from './VideoItem';
 
-const VideoList = () => {
-    return (<h1>VideoList Component</h1>)
+const VideoList = ({ videos, onVideoSelect }) => {
+    const listOfVideos = videos.map((video, id) => <VideoItem onVideoSelect={onVideoSelect} key={id} video={video} />);
+
+    return (
+        <Grid container spacing={10}>
+            {listOfVideos}
+        </Grid>
+    )
 }
 
 export default VideoList;
