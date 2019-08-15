ff362b7c6c297d83af462ccd0984bc98b4ed679a
Chris Otto <chris.otto6@gmail.com>
2019-08-14 21:15:41 -0500

    Finishing the VideoDetail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Paper, TextField, Typography } from '@material-ui/core';
 
-const VideoDetail = () => {
-    return (<h1>VideoDetail Component</h1>)
+const VideoDetail = ({ video }) => {
+    if(!video) {
+        return <div>Loading...</div>
+    }
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
+    return (
+        <React.Fragment>
+            <Paper elevation={6} style={{ height: '70%' }}>
+                <iframe frameBorder="0" height="100%" width="100%" title="Video Player" src={videoSrc}/>
+            </Paper>
+            <Paper elevation={6} style={{ padding: '15px' }}>
+                <Typography variant="h4">{video.snippet.title} - {video.snippet.channelTitle}</Typography>
+                <Typography variant="subtitle1">{video.snippet.channelTitle}</Typography>
+                <Typography variant="subtitle2">{video.snippet.description}</Typography>
+            </Paper>
+        </React.Fragment>
+    )
 }
 
 export default VideoDetail;
