d96ee07594f80bc0363ffeb70c9dc47a4f5fb3ca
Chris Otto <chris.otto6@gmail.com>
2019-08-14 21:16:49 -0500

    Updating the app.js to use all the components and finish the app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import youtube from './api/youtube'
 import { Grid } from '@material-ui/core';
+import { SearchBar, VideoList, VideoDetail } from './components';
+import youtube from './api/youtube'
+require('dotenv').config();
 
 class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  }
+
+handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyBMPokeSyvYrYXROnJjE5WMkourtG3bx2E',
+        q: searchTerm,
+      }
+    });
+
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+  }
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  }
+
 	render() {
+    const { selectedVideo, videos } = this.state;
+
       return (
         <Grid style={{ justifyContent: 'center' }} container spacing={10}>
           <Grid item xs={11}>
             <Grid container spaciyng={10}>
               <Grid item xs={12}>
-              	{/* This is where SearchBar component will go */}
+              	<SearchBar onFormSubmit={this.handleSubmit}/>
               </Grid>
               <Grid item xs={8}>
-              	{/* This is where VideoDetail component will go */}
+              	<VideoDetail video={selectedVideo}/>
               </Grid>
               <Grid item xs={4}>
-              	{/* This is where VideoList component will go */}
+              	<VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
               </Grid>
             </Grid>
           </Grid>
